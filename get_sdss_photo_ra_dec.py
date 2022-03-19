4253841700baf7abac1fd2382c0de2009783cc55
haifengchengguang <60420171+haifengchengguang@users.noreply.github.com>
2022-03-19 19:27:54 +0800

    fix:计算卷积

---
+++ get_sdss_photo_ra_dec.py
@@ -0,0 +1,55 @@
+import numpy as np
+# from astropy.io.fits import tabledump
+import pandas as pd
+from astroquery.sdss import SDSS
+# specs = SDSS.get_spectra(plate=328, mjd=52282,fiberID=254,data_release=17)
+from tqdm import tqdm
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+
+fileName="E:\\学习资料\\天文\\作业五\\20211123_150w_match_simbad\\200wLmore15.csv"
+#Lmore15=np.loadtxt(fileName,delimiter=',',dtype=int,usecols=np.arange(3),skiprows=1)   #跳过第4938行
+Lmore15=pd.read_csv(r"E:\\学习资料\\天文\\作业五\\20211123_150w_match_simbad\\200wLmore15.csv")
+#print(Lmore15)
+# #tabledump('321020868929021952.fits',specs)   #将获取的数据写入文件
+# specs[0].writeto('test.fits')
+from astropy.io import fits
+# testspec=fits.open('test.fits')      #读取文件
+for everyrow in tqdm(Lmore15.iterrows()):
+    #print('ra=',everyrow[1][0],'dec=',everyrow[1][1])
+    ra=everyrow[1][0]
+    dec=everyrow[1][1]
+    #print(dec)
+    objid=everyrow[1][2]
+    c = SkyCoord(ra=ra * u.degree, dec=dec * u.degree, frame='icrs')
+    #print(plate,mjd,fiberid)
+
+    #specs = SDSS.get_spectra(coordinates=c,radius=(1/1200)*u.degree,data_release=17)    #获取每一行的数据
+    photo=SDSS.get_images(coordinates=c,radius=(1/1200)*u.degree)
+    # if specs is None:
+    #     print()
+    # else:
+    #     specs[0].writeto('G:\sdss光谱\match1\spec_'+str(objid)+'.fits')
+    #     print('ra:', ra, 'dec:', dec, 'objid:', objid)
+#     #specs[0].writeto('E:\学习资料\天文\作业五\sdss光谱\spec_'+str(plate)+'_'+str(mjd)+'_'+str(fiberid)+'.fits')
+
+#     #print(specs[0])
+#     #print(specs[0].header)
+#     #print(specs[0].data)
+#     #print(specs[0].header['OBJID'])
+#     #print(specs[0].header['PLATE'])
+#     #print(specs[0].header['MJD'])
+#     #print(specs[0].header['FIBERID'])
+#     #print(specs[0].header['RA'])
+#     #print(specs[0].header['DEC'])
+#     #print(specs[0].header['Z'])
+#     #print(specs[0].header['ZWARNING'])
+#     #print(specs[0].header['SPECZ'])
+#     #print(specs[0].header['SPECZ_IVAR'])
+#     #print(specs[0].header['SPECZ_EW'])
+#     #print(specs[0].header['SPECZ_EW_IVAR'])
+#     #print(specs[0].header['SPECZ_RCHI2'])
+#     #print(specs[0].header['SPECZ_RCHI2_IVAR'])
+#     #print(specs[0].header['SPECZ_RCHI2_R'])
+#     #print(specs[0].header['SPECZ_RCHI2_R_IVAR'])
+    #specs[0].writeto('sdss_fits/plate'+str(plate)+'_mjd'+str(mjd)+'_fiberid'+str(fiberid)+'.fits')
--- phx_zeropoint.py
+++ phx_zeropoint.py
@@ -1,3 +1,4 @@
+import math
 import os
 
 import numpy as np
@@ -79,107 +80,44 @@ camera_W4=pd.read_csv(r'E:\学习资料\天文\作业五\20211219_光谱响应�
 camera_W4_flux=camera_W4['RSR']
 camera_W4_wavelength=camera_W4['W']
 
+def caculate_mag(camera_wavelength,camera_flux,mag_name):
+    mag_full=0
+    for i in range(len(camera_wavelength) - 1):
+        waveint = int(camera_wavelength[i] * 1000000)
+        temp = np.argwhere(x == waveint)
+        x_index = temp
+        # print("x_index:",x_index)
+        if len(x_index) == 0: x_index = (np.abs(x - waveint)).argmin()
+        x_index = int(x_index)
+        mag_full += camera_flux[i] * dst[x_index] * (camera_wavelength[i + 1] - camera_wavelength[i])
+    mag = -2.5 * math.log((mag_full / ((max(camera_wavelength) - min(camera_wavelength)))), 10)
+    print(mag,end='')
+print("fitsname,rmag,imag,zmag,Jmag,Hmag,Kmag,W1mag,W2mag,W3mag,W4mag")
 for filename in phxpath:
     readspec = readphx(filename)
     x = readspec[0]
     # print(x)
     y = readspec[1]
-    r_mag_full=0
-    i_mag_full = 0
-    z_mag_full = 0
     dst = cv2.GaussianBlur(src=y, ksize=(25, 25), sigmaX=6)
-    for i in range(len(camera_r_wavelength)-1):
-        waveint = int(camera_r_wavelength[i])
-        x_index = int(np.argwhere(x == waveint))
-        #print("x_index:",x_index)
-        r_mag_full += camera_r_flux[i] * dst[x_index] * 25
-    rmag= r_mag_full / (max(camera_r_wavelength) - min(camera_r_wavelength))
-    print(filename+"----rmag="+str(rmag))
-    for i in range(len(camera_i_wavelength)-1):
-        waveint = int(camera_i_wavelength[i])
-        x_index = int(np.argwhere(x == waveint))
-        #print("x_index:",x_index)
-        i_mag_full += camera_i_flux[i] * dst[x_index] * 25
-    imag= i_mag_full / (max(camera_i_wavelength) - min(camera_i_wavelength))
-    print(filename + "----imag=" + str(imag))
-    for i in range(len(camera_z_wavelength)-1):
-        waveint = int(camera_z_wavelength[i])
-        x_index = int(np.argwhere(x == waveint))
-        #print("x_index:",x_index)
-        z_mag_full += camera_z_flux[i] * dst[x_index] * 25
-    zmag= z_mag_full / (max(camera_z_wavelength) - min(camera_z_wavelength))
-    print(filename + "----zmag=" + str(zmag))
-    J_mag_full=0
-    for i in range(len(camera_J_wavelength)-1):
-        waveint = int(camera_J_wavelength[i]*1000000)
-        x_index = int(np.argwhere(x == waveint))
-        #print("x_index:",x_index)
-        J_mag_full += camera_J_flux[i] * dst[x_index] * (camera_J_wavelength[i+1]-camera_J_wavelength[i])
-    Jmag= J_mag_full / ((max(camera_J_wavelength) - min(camera_J_wavelength)))
-    print(filename + "----Jmag=" + str(Jmag))
-    H_mag_full = 0
-    for i in range(len(camera_H_wavelength) - 1):
-        waveint = int(camera_H_wavelength[i] * 1000000)
-        x_index = int(np.argwhere(x == waveint))
-        # print("x_index:",x_index)
-        H_mag_full += camera_H_flux[i] * dst[x_index] * (camera_H_wavelength[i + 1] - camera_H_wavelength[i])
-    Hmag = H_mag_full / ((max(camera_H_wavelength) - min(camera_H_wavelength)))
-    print(filename + "----Hmag=" + str(Hmag))
-    K_mag_full = 0
-    for i in range(len(camera_K_wavelength) - 1):
-        waveint = int(camera_K_wavelength[i] * 1000000)
-        temp=np.argwhere(x == waveint)
-        x_index = temp
-        #print("x_index:",x_index)
-        if len(x_index)==0:x_index=(np.abs(x-waveint)).argmin()
-        x_index = int(x_index)
-        K_mag_full += camera_K_flux[i] * dst[x_index] * (camera_K_wavelength[i + 1] - camera_K_wavelength[i])
-    Kmag = K_mag_full / ((max(camera_K_wavelength) - min(camera_K_wavelength)))
-    print(filename + "----Kmag=" + str(Kmag))
-    W1_mag_full = 0
-    for i in range(len(camera_W1_wavelength) - 1):
-        waveint = int(camera_W1_wavelength[i] * 1000000)
-        #print(waveint)
-        temp = np.argwhere(x == waveint)
-        x_index = temp
-        # print("x_index:",x_index)
-        if len(x_index) == 0: x_index = (np.abs(x - waveint)).argmin()
-        #print("x_index:", x_index)
-        x_index=int(x_index)
-        W1_mag_full += camera_W1_flux[i] * dst[x_index] * (camera_W1_wavelength[i + 1] - camera_W1_wavelength[i])
-    W1mag = W1_mag_full / ((max(camera_W1_wavelength) - min(camera_W1_wavelength)))
-    print(filename + "----W1mag=" + str(W1mag))
-    W2_mag_full = 0
-    for i in range(len(camera_W2_wavelength) - 1):
-        waveint = int(camera_W2_wavelength[i] * 1000000)
-        temp = np.argwhere(x == waveint)
-        x_index = temp
-        # print("x_index:",x_index)
-        if len(x_index) == 0: x_index = (np.abs(x - waveint)).argmin()
-        x_index = int(x_index)
-        W2_mag_full += camera_W2_flux[i] * dst[x_index] * (camera_W2_wavelength[i + 1] - camera_W2_wavelength[i])
-    W2mag = W2_mag_full / ((max(camera_W2_wavelength) - min(camera_W2_wavelength)))
-    print(filename + "----W2mag=" + str(W2mag))
-    W3_mag_full = 0
-    for i in range(len(camera_W3_wavelength) - 1):
-        waveint = int(camera_W3_wavelength[i] * 1000000)
-        temp = np.argwhere(x == waveint)
-        x_index = temp
-        # print("x_index:",x_index)
-        if len(x_index) == 0: x_index = (np.abs(x - waveint)).argmin()
-        x_index = int(x_index)
-        W3_mag_full += camera_W3_flux[i] * dst[x_index] * (camera_W3_wavelength[i + 1] - camera_W3_wavelength[i])
-    W3mag = W3_mag_full / ((max(camera_W3_wavelength) - min(camera_W3_wavelength)))
-    print(filename + "----W3mag=" + str(W3mag))
-    W4_mag_full = 0
-    for i in range(len(camera_W4_wavelength) - 1):
-        waveint = int(camera_W4_wavelength[i] * 1000000)
-        temp = np.argwhere(x == waveint)
-        x_index = temp
-        # print("x_index:",x_index)
-        if len(x_index) == 0: x_index = (np.abs(x - waveint)).argmin()
-        x_index = int(x_index)
-        W4_mag_full += camera_W4_flux[i] * dst[x_index] * (camera_W4_wavelength[i + 1] - camera_W4_wavelength[i])
-    W4mag = W4_mag_full / ((max(camera_W4_wavelength) - min(camera_W4_wavelength)))
-    print(filename + "----W4mag=" + str(W4mag))
-    break
+    print(filename,end=',')
+    caculate_mag(camera_r_wavelength,camera_r_flux,"rmag")
+    print(end=',')
+    caculate_mag(camera_i_wavelength, camera_i_flux, "imag")
+    print(end=',')
+    caculate_mag(camera_z_wavelength, camera_z_flux, "zmag")
+    print(end=',')
+    caculate_mag(camera_J_wavelength, camera_J_flux, "Jmag")
+    print(end=',')
+    caculate_mag(camera_H_wavelength, camera_H_flux, "Hmag")
+    print(end=',')
+    caculate_mag(camera_K_wavelength, camera_K_flux, "Kmag")
+    print(end=',')
+    caculate_mag(camera_W1_wavelength, camera_W1_flux, "W1mag")
+    print(end=',')
+    caculate_mag(camera_W2_wavelength, camera_W2_flux, "W2mag")
+    print(end=',')
+    caculate_mag(camera_W3_wavelength, camera_W3_flux, "W3mag")
+    print(end=',')
+    caculate_mag(camera_W4_wavelength, camera_W4_flux, "W4mag")
+    print()
+print("end")
